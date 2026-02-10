dee9732104ecfa43e38b7441e74c1c09a9f6977c
alinahmettekin <alinahmettekin@gmail.com>
2026-02-10 10:55:39 +0300

    pay

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'web.simart.cloud',
       },
       {
         protocol: 'http',
